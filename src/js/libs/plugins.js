56fef9370c1c60c46bbf7d895763e2743dd17f71
Silver <silver22@xaker.ru>
2015-05-13 16:35:34 +0300

    Clear main js and add short description

--- src/js/libs/plugins.js
+++ src/js/libs/plugins.js
@@ -4,8 +4,5 @@
 //Magnific popup
 //= ../../../builder/bower_components/magnificpopup/dist/jquery.magnific-popup.js
 
-//JQuery countdown
-//= jquery.countdown.min.js
-
 //JQuery Mask
 //= jquery.maskedinput.min.js
