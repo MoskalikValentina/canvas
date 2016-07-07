09498ead0d7349eb04b3c0bb706e6cf5f30901cb
bohdannov <bogdannov@i.ua>
2016-07-07 13:56:32 +0300

    merge and add jshint

--- resources/assets/ts/main.ts
+++
@@ -1,4 +0,0 @@
-import {bootstrap}    from '@../../gulp-builder/bower_components/angular2-build/angular/platform-browser-dynamic';
-import {AppComponent} from './app.component';
-
-bootstrap(AppComponent);
